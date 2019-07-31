a17d009ca22cd91a270335b72a55e3834b568bbf
Jason Loomis <jason@jrloomis.com>
2019-07-30 20:27:22 -0400

    added header and navigation layouts

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Astronomy Picture of the Day</h1>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Nav.scss";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <button className="nav__btn">button</button>
+      <button className="nav__btn">button</button>
+      <button className="nav__btn">button</button>
+      <button className="nav__btn">button</button>
+    </nav>
+  );
+};
+
+export default Nav;
