df97bd615077735c70417641cf8afcaa3e5d653a
Jason Loomis <jason@jrloomis.com>
2019-07-30 22:53:31 -0400

    removed navigation - didn't need it

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Nav } from "../Nav/Nav";
 import "./Header.scss";
 
-const Header = () => {
+const Header = props => {
   return (
     <header className="header">
-      <h1 className="header__title">Astronomy Picture of the Day</h1>
-      <Nav />
+      <h1 className="header__title">Astronomy Picture of the Day for {props.date}</h1>
     </header>
   );
 };
