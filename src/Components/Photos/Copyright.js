1e471e145f1d54ed66bc751661a6aa1f7daeee27
Jason Loomis <jason@jrloomis.com>
2019-08-01 22:39:35 -0400

    refactored for semantic-ui

--- src/Components/Photos/Copyright.js
+++ src/Components/Photos/Copyright.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
-export default function Copyright(props) {
-  return <div className="display__copyright">&copy; {props.copyright}</div>;
-}
+// export default function Copyright(props) {
+// return <div className="display__copyright">&copy; {props.copyright}</div>;
+// }
+
+const Copyright = props => {
+  if (props.hasOwnProperty("copyright")) {
+    return (
+      <Container
+        style={{
+          marginTop: "4rem"
+        }}
+      >
+        {props.copyright}
+      </Container>
+    );
+  } else {
+    return "";
+  }
+};
+
+export default Copyright;
