8a913e469f9cbf897deb65dd446d27f7d59e7247
Jason Loomis <jason@jrloomis.com>
2019-08-01 21:30:48 -0400

    refactored for semantic-ui-react

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,40 @@
 import React from "react";
-import "./Header.scss";
+// import "./Header.scss";
+import { Container, Header } from "semantic-ui-react";
 
-const Header = props => {
+// const Header = props => {
+// return (
+// <header className="header">
+
+/* <h1 className="header__title">Astronomy Picture of the Day for {props.date}</h1> */
+
+/* </header> */
+
+// );
+// };
+//
+// export default Header;
+
+const headerContainer = props => {
   return (
-    <header className="header">
-      <h1 className="header__title">Astronomy Picture of the Day for {props.date}</h1>
-    </header>
+    <Container
+      style={{
+        alignSelf: "flex-start"
+      }}
+    >
+      <Header
+        as="h1"
+        style={{
+          marginBottom: "2rem",
+          padding: "1rem",
+          fontSize: "3rem",
+          fontWeight: "100"
+        }}
+      >
+        Astronomy Picture of the Day for {props.date}
+      </Header>
+    </Container>
   );
 };
 
-export default Header;
+export default headerContainer;
