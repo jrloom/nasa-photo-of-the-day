f32ddfc653638da0cd22b30be8d8cb391183a3e3
Jason Loomis <jason@jrloomis.com>
2019-08-01 22:11:00 -0400

    refactored for semantic-ui

--- src/Components/Photos/Title.js
+++ src/Components/Photos/Title.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Header } from "semantic-ui-react";
 
-export default function Title(props) {
-  return <h2 className="display__title">{props.title}</h2>;
-}
+// export default function Title(props) {
+// return <h2 className="display__title">{props.title}</h2>;
+// }
+
+const Title = props => {
+  return (
+    <Header
+      as="h2"
+      style={{
+        marginBottom: "3rem",
+        fontSize: "2rem"
+      }}
+    >
+      {props.title}
+    </Header>
+  );
+};
+
+export default Title;
