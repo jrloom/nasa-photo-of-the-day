6c6bdeadcbf389d61287915edbcb4c6c765a800f
Jason Loomis <jason@jrloomis.com>
2019-08-01 22:11:39 -0400

    refactored for semantic-ui

--- src/Components/Photos/Img.js
+++ src/Components/Photos/Img.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Image } from "semantic-ui-react";
 
-export default function Img(props) {
-  return <img className="display__img" src={props.url} alt="SPACE!" />;
-}
+// export default function Img(props) {
+// return <img className="display__img" src={props.url} alt="SPACE!" />;
+// }
+
+const Img = props => {
+  return (
+    <Image
+      src={props.url}
+      alt="SPACE!"
+      style={{
+        height: "50rem",
+        maxHeight: "50rem"
+      }}
+    />
+  );
+};
+
+export default Img;
